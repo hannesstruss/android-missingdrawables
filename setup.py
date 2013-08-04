292ffbdbce0bc4667c69f5d97554ba5257f91538
Hannes Struss <x@hannesstruss.de>
2013-08-04 18:43:06 +0200

    MANIFEST.in and more setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,24 @@ from distutils.core import setup
 
 setup(
     name='android-missingdrawables',
-    version='0.1',
+    version='1.0',
     description='Easily find out for which density buckets your drawables are missing.',
+    license='MIT',
     author='Hannes Struss',
     author_email='x@hannesstruss.de',
     url='https://github.com/hannesstruss/android-missingdrawables',
     py_modules=['missingdrawables'],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
     entry_points={
         'console_scripts': [
             'missingdrawables=missingdrawables:main',
